db21aec1cda1a1608eb8089e9067273983790219
Rafael Carvalho <rafaelcarvalho.rl@gmail.com>
2023-02-22 20:51:27 -0400

    Practicing variables scopes

--- scope.js
+++ scope.js
@@ -1,12 +1,45 @@
 // Global Scope
+
 var name = "Carvalho"
+const PI = 3.1415
 
-// Block Scope
-{
+    // Block Scope
+    {
+        let name = "Rafael"
+        console.log(name) // this variable can't be accessed from outside because it was declared with let
 
-    let name = "Rafael"
-    console.log(name)
-}
+        const PI = 3.14 // can't be accessed from outside because it was declared with const
+    }
 
 // Global
 console.log(name)
+console.log(PI)
+
+    // Local Scope
+
+    function hello(){
+        let person = "Rafa"
+        console.log(person)
+    }
+
+let person = "Me"
+console.log(person)
+
+    function hi(){
+        console.log(person)
+    }
+    hi()
+
+    // console.log(myName)
+
+    // function hey(){
+    //     myName = "Rafaelzito"
+    // }
+
+
+    
+
+
+
+
+

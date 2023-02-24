6867e9774b7a78d2ca18f52ca4433a4a394678ff
Rafael Carvalho <rafaelcarvalho.rl@gmail.com>
2023-02-24 17:45:50 -0400

    arrow function study

---
+++ arrow-function.js
@@ -0,0 +1,18 @@
+function hello(name){
+    console.log(name)
+}
+
+hi = (realName) => {
+    console.log(realName)
+}
+
+hey = (nickName) => console.log(nickName)
+
+const studentName = "Rafael"
+const studentRealName = "Rafael Carvalho"
+const studentNickName = "Rafa"
+
+hello(studentName)
+hi(studentRealName)
+hey(studentNickName)
+

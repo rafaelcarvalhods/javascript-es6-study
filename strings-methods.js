1a44cb5ac6e5635fd2835931998fb18731fd9da8
Rafael Carvalho <rafaelcarvalho.rl@gmail.com>
2023-02-27 08:24:04 -0400

    string methods study

---
+++ strings-methods.js
@@ -0,0 +1,19 @@
+let text = "JavaScript is a great programming language!"
+
+// Check if 'text' string starts with 'JavaScript'
+if(text.startsWith("JacaScript")){
+    console.log("The string begins with the word 'JavaScript'.")
+} else {
+    console.log("The word wasn't found.")
+}
+
+
+// Check if 'text' string ends with '!'
+console.log(text.endsWith("!"))
+
+// Check if the string contains the word 'programming'
+console.log(text.includes("programming"))
+
+
+//Use this method to repeat a string as many times as you want
+console.log("Ha".repeat(10))

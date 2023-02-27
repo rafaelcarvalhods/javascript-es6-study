45ec1756257c0d374ebc1f1180b56d5626a8d2af
Rafael Carvalho <rafaelcarvalho.rl@gmail.com>
2023-02-27 09:27:15 -0400

    complementing array methods study

--- array-methods.js
+++ array-methods.js
@@ -11,6 +11,22 @@ console.log(array)
 const list = [5, 3, -1, 0, -5]
 console.log(list.find(x => x < 0))
 
+const data = [
+    {
+        name: "Rafael",
+        lastName: "Silva",
+        linkedin: "@rafaelcarvalhods"
+    },
+
+    {
+        name: "Marcia",
+        lastName: "Reis",
+        linkedin: "@marciareis"
+    }
+]
+
+console.log(data.find(item => item.name == "Marcia"))
+
 // array.findIndex - returns the index of the first value that satisfies the function requirements
 console.log(list.findIndex(x => x < 0))
 

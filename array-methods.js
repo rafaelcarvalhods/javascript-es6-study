99b81623d50750ef141e6dfbdfbd65dbc7851f27
Rafael Carvalho <rafaelcarvalho.rl@gmail.com>
2023-02-27 21:03:37 -0400

    Array and String methods review

--- array-methods.js
+++ array-methods.js
@@ -1,43 +1,61 @@
-const text = "Rafael"
+// const text = "Rafael"
 
-// Array.from
-console.log(Array.from(text))
+// // Array.from
+// console.log(Array.from(text))
 
-// Array.of - creates an array from several elements
-const array = Array.of(1, 2, "Name", {name: 'Rafael'})
-console.log(array)
+// // Array.of - creates an array from several elements
+// const array = Array.of(1, 2, "Name", {name: 'Rafael'})
+// console.log(array)
 
-// array.find - returns the first value that satisfies the function requirements
-const list = [5, 3, -1, 0, -5]
-console.log(list.find(x => x < 0))
+// // array.find - returns the first value that satisfies the function requirements
+// const list = [5, 3, -1, 0, -5]
+// console.log(list.find(x => x < 0))
 
-const data = [
-    {
-        name: "Rafael",
-        lastName: "Silva",
-        linkedin: "@rafaelcarvalhods"
-    },
+// const data = [
+//     {
+//         name: "Rafael",
+//         lastName: "Silva",
+//         linkedin: "@rafaelcarvalhods"
+//     },
 
-    {
-        name: "Marcia",
-        lastName: "Reis",
-        linkedin: "@marciareis"
-    }
-]
+//     {
+//         name: "Marcia",
+//         lastName: "Reis",
+//         linkedin: "@marciareis"
+//     }
+// ]
 
-console.log(data.find(item => item.name == "Marcia"))
+// console.log(data.find(item => item.name == "Marcia"))
 
-// array.findIndex - returns the index of the first value that satisfies the function requirements
-console.log(list.findIndex(x => x < 0))
+// // array.findIndex - returns the index of the first value that satisfies the function requirements
+// console.log(list.findIndex(x => x < 0))
 
 
-// Array.fill
-const arr = new Array(10)
-arr.fill("Name")
-console.log(arr)
+// // Array.fill
+// const arr = new Array(10)
+// arr.fill("Name")
+// console.log(arr)
 
-arr.fill("Rafael", 0, 1)
-console.log(arr)
+// arr.fill("Rafael", 0, 1)
+// console.log(arr)
 
-arr.fill("Marcia", 2, 3)
-console.log(arr)
+// arr.fill("Marcia", 2, 3)
+// console.log(arr)
+
+// Review
+
+const test = Array.from("What is impossible?")
+console.log(test)
+
+const mixedList = Array.of(3, 4, "My name", {date: "27/02/2023"})
+console.log(mixedList)
+
+const newList = [3, 4, 7, -2, -1, -5]
+let item = newList.find(x => x < 0)
+console.log(item)
+console.log(newList.findIndex(x => x < 0))
+
+const newArr = new Array(7)
+newArr.fill("Name", 0, 4)
+newArr.fill("Last Name", 4, 7)
+console.log(newArr)
--- strings-methods.js
+++ strings-methods.js
@@ -17,3 +17,14 @@ console.log(text.includes("programming"))
 
 //Use this method to repeat a string as many times as you want
 console.log("Ha".repeat(10))
+
+
+// Review
+
+const str = "King of my heart!"
+
+console.log(str.startsWith("King"))
+console.log(str.endsWith("heart"))
+console.log(str.includes("my"))
+console.log("You are!".repeat(7))
+

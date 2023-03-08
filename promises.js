1b8dae615006959d42fb263f3871948b4d7efacf
Rafael Carvalho <rafaelcarvalho.rl@gmail.com>
2023-03-07 20:22:01 -0400

    Add node-fetch

--- promises.js
+++ promises.js
@@ -13,3 +13,16 @@ myPromise.then((data) => {
     console.log(data)
 })
 
+// using Fetch request with Github API
+
+const userName = "rafaelcarvalhods";
+
+fetch(`https://api.github.com/users/${userName}`, {
+    method: 'GET',
+    headers: {
+        Accept: 'application/vnd.github.v3+json',
+    }
+}).then((response) => {
+    console.log(typeof response)
+})
+

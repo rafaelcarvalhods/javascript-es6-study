4d65551c23222c5338689b8b240c4324c1b5f6ef
Rafael Carvalho <rafaelcarvalho.rl@gmail.com>
2023-03-14 12:05:44 -0400

    Fetch studying

--- promises.js
+++ promises.js
@@ -1,11 +1,12 @@
 import fetch from 'node-fetch'
+import 
 
 const myPromise = new Promise((resolve, reject) => {
 
     const oneName = "Rafael"
 
     if(oneName === "Rafael"){
-        resolve("I got it!")
+        setTimeout(() => resolve("I got it!"), 2000)
     } else {
         reject("Houston, we've got a problem!")
     }
@@ -15,14 +16,17 @@ myPromise.then((data) => console.log(data)).catch((err) => console.log(err))
 
 // using Fetch request with Github API
 
-// const userName = "rafaelcarvalhods";
+const userName = "rafaelcarvalhods";
 
-// fetch(`https://api.github.com/users/${userName}`, {
-//     method: 'GET',
-//     headers: {
-//         Accept: 'application/vnd.github.v3+json',
-//     },
-// }).then((response) => {
-//     console.log(typeof response)
-// })
+fetch(`https://api.github.com/users/${userName}`, {
+    method: 'GET',
+    headers: {
+        Accept: 'application/vnd.github.v3+json',
+    },
+}).then((response) => {
+    console.log(typeof response)
+})
+
+const list = fetch('/home/rafaelcarvalhods/Desktop/Dev/javascript-es6-study/list.json')
+list.then((item) => console.log(item))
 

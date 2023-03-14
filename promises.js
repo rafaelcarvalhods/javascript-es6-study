4b9c716892db86b56060c74f05a3a1788ffd37c5
Rafael Carvalho <rafaelcarvalho.rl@gmail.com>
2023-03-14 08:45:01 -0400

    Practicing Promises

--- promises.js
+++ promises.js
@@ -1,30 +1,28 @@
-import fetch from "node-fetch";
+import fetch from 'node-fetch'
 
 const myPromise = new Promise((resolve, reject) => {
-    
-    const name = "Rafael"
 
-    if(name === "Rafael"){
-        resolve("User found!")
+    const oneName = "Rafael"
+
+    if(oneName === "Rafael"){
+        resolve("I got it!")
     } else {
-        reject("User not found")
+        reject("Houston, we've got a problem!")
     }
 })
 
-myPromise.then((data) => {
-    console.log(data)
-})
+myPromise.then((data) => console.log(data)).catch((err) => console.log(err))
 
 // using Fetch request with Github API
 
-const userName = "rafaelcarvalhods";
+// const userName = "rafaelcarvalhods";
 
-fetch(`https://api.github.com/users/${userName}`, {
-    method: 'GET',
-    headers: {
-        Accept: 'application/vnd.github.v3+json',
-    },
-}).then((response) => {
-    console.log(typeof response)
-})
+// fetch(`https://api.github.com/users/${userName}`, {
+//     method: 'GET',
+//     headers: {
+//         Accept: 'application/vnd.github.v3+json',
+//     },
+// }).then((response) => {
+//     console.log(typeof response)
+// })
 
